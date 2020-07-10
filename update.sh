91dbbab09c43e1bae5254a77feda05b33116f3f7
py2iv <68090867+py2iv@users.noreply.github.com>
2020-07-10 16:36:11 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
-systemctl stop pistar-watchdog
-systemctl stop mmdvmhost
+#####################################################
+#                                                   #
+#    Install libs required to build and install     #
+#            for the STM32 - PY2IV                  #
+#                                                   #
+#####################################################
+
+pistar-watchdog.service stop
+systemctl stop mmdvmhost.timer
+systemctl stop mmdvmhost.service
 apt-get update
 apt-get install -y -t git binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi libstdc++-arm-none-eabi-newlib autoconf libtool pkg-config libusb-1.0-0 libusb-1.0-0-dev stm32flash
 cd /opt/
