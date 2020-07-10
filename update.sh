9f10a72c9859bcd6537e0aae6efd76defa9f72bf
py2iv <68090867+py2iv@users.noreply.github.com>
2020-07-09 23:38:04 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y -t git binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi libstdc++-arm-none-eabi-newlib autoconf libtool pkg-config libusb-1.0-0 libusb-1.0-0-dev stm32flash
 systemctl stop pistar-watchdog
-cd /opt/src
+cd /opt/
 git clone https://github.com/g4klx/MMDVM.git
-cd MMDVM/
+cd /opt/MMDVM/
 git clone https://github.com/shawnchain/STM32F10X_Lib.git
