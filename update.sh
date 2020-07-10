7db37fc6e5acf98dbdf8cd89331c68099a36ace3
py2iv <68090867+py2iv@users.noreply.github.com>
2020-07-09 23:34:14 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 apt-get update
-apt-get install -y -t git binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi libstdc++-arm-none-eabi-newlib autoconf libtool pkg-config libusb-1.0-0 libusb-1.0-0-dev
+apt-get install -y -t git binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi libstdc++-arm-none-eabi-newlib autoconf libtool pkg-config libusb-1.0-0 libusb-1.0-0-dev stm32flash
 systemctl stop pistar-watchdog
 cd /opt/src
 git clone https://github.com/g4klx/MMDVM.git
