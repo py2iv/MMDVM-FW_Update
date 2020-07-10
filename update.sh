1175f91d58b9f5db91d983ad16c1d9264e11f5d2
py2iv <68090867+py2iv@users.noreply.github.com>
2020-07-10 17:57:23 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,6 +12,7 @@ systemctl stop mmdvmhost.service
 apt-get update
 apt-get install -y binutils-arm-none-eabi gcc-arm-none-eabi gdb-arm-none-eabi libstdc++-arm-none-eabi-newlib autoconf libtool pkg-config libusb-1.0-0 libusb-1.0-0-dev stm32flash
 cd /opt/
-git clone https://github.com/g4klx/MMDVM.git
+#git clone https://github.com/g4klx/MMDVM.git
+https://github.com/N4IRS/MMDVM-Install.git
 cd /opt/MMDVM/
 git clone https://github.com/shawnchain/STM32F10X_Lib.git
